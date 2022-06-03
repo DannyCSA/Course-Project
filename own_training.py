6da11048022d111e50e8081f6f07d5d748aec672
DannyCSA <105173243+DannyCSA@users.noreply.github.com>
2022-06-03 09:05:43 -0400

    Add files via upload

---
+++ own_training.py
@@ -0,0 +1,5 @@
+import torch
+import os
+from IPython.display import Image, clear_output  # to display images
+
+print(f"Setup complete. Using torch {torch.__version__} ({torch.cuda.get_device_properties(0).name if torch.cuda.is_available() else 'CPU'})")
